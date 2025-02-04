ec2d955c834ae2e7d6652d7ca632fe563c11ee6d
jmlc643 <josemarialuyocampos@gmail.com>
2025-02-04 00:37:31 -0500

    Sidebar finished and add new pages to test router active link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PopularComponent } from './pages/popular/popular.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'history', component: HistoryComponent },
+    { path: 'popular', component: PopularComponent },
+    { path: 'categories', component: CategoriesComponent },
 ];
--- src/app/sections/sidebar/sidebar.component.ts
+++ src/app/sections/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
