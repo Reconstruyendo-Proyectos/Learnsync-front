3ca3be191b30c7bf571d3da835703709f36bd5ce
jmlc643 <josemarialuyocampos@gmail.com>
2025-02-15 01:58:49 -0500

    Login Component Finished and Fix Bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { SidebarComponent } from "./sections/sidebar/sidebar.component";
 import { SidebarService } from './sections/sidebar/service/sidebar.service';
@@ -14,11 +14,18 @@ import { CommonModule } from '@angular/common';
 export class AppComponent implements OnInit {
   title = 'learnsync';
   isSidebarOpen = false;
+  isAuthRoute = false;
   sidebarService = inject(SidebarService)
+  router = inject(Router)
 
   ngOnInit(): void {
     this.sidebarService.isOpen$.subscribe(isOpen => {
       this.isSidebarOpen = isOpen ?? false;
     });
+
+    this.router.events.subscribe(() => {
+      const currentRoute = this.router.url;
+      this.isAuthRoute = ['/login', '/register', '/forgot-password'].includes(currentRoute);
+    })
   }
 }
