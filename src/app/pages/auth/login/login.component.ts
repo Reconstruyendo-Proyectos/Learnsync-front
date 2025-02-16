782ce7692c6ce367a9e0da7ccc8ee42219db481a
jmlc643 <josemarialuyocampos@gmail.com>
2025-02-16 00:02:39 -0500

    Finished Register Component and Add Modal Component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,19 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { SidebarService } from '../../../sections/sidebar/service/sidebar.service';
 import { InputComponent } from "../../../components/input/input.component";
 import { ButtonComponent } from "../../../components/button/button.component";
 import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [InputComponent, ButtonComponent, RouterModule],
+  imports: [InputComponent, ButtonComponent, RouterModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
-  sidebarService = inject(SidebarService);
+  showImage = true;
 
   ngOnInit() {
-      this.sidebarService.closeSidebar();
+      this.checkScreenWidth();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenWidth();
+  }
+
+  private checkScreenWidth(): void {
+    this.showImage = window.innerWidth > 768;
   }
 }
+
